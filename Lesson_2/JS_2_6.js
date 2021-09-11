51226c1c650155a159ce4e99c3bf4b0ea39a2be3
Kolodinsky <Photon.74@gmail.com>
2021-09-11 16:02:11 +0300

    Шестое задание урока №2

---
+++ Lesson_2/JS_2_6.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let money = parseInt(prompt("Сколько денег вы хотите положить на счет?"));
+alert(`Ваша сумма ${money} ${getCorrectWord(money)} успешно зачислена.`);
+
+/**
+ * Принимает число и возвращает слово "рубль" в правильном падеже
+ * @param {number} a сумма
+ * @returns слово в правильном падеже
+ */
+function getCorrectWord(a) {
+    const b = String(a);
+    const lastChar = +b.charAt(b.length - 1);
+
+    if (a < 5 || a > 20) {
+        if (lastChar === 1) return "рубль";
+        if (lastChar > 1 && lastChar <= 4) return "рубля";
+    }
+    return "рублей";
+}
