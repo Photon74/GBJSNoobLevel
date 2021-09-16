c3eac8e850f119bf9a62d0c7af0d92f211986bd8
Kolodinsky <Photon.74@gmail.com>
2021-09-16 17:33:14 +0300

    Внес исправления

--- Lesson_3/Scripts/JS_3_4.js
+++ Lesson_3/Scripts/JS_3_4.js
@@ -27,19 +27,10 @@ const products = [
     },
 ];
 
-let products1 = products.filter(product => {
-    if ('photos' in product && product.photos.length > 0) {
-        return true;
-    }
-    return false;
-});
+let products1 = products.filter(product => 'photos' in product && product.photos.length > 0);
 console.log(products1);
 
 
 
-let products2 = products.sort(function (a, b) {
-    if (a.price > b.price) return 1;
-    if (a.price < b.price) return -1;
-    return 0;
-});
+let products2 = products.sort((a, b) => a.price - b.price);
 console.log(products2);
