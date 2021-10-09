9c372348ab54d00eb0a2a456074d28b3bd6e2535
Aleksandr <Photon.74@gmail.com>
2021-10-10 01:29:16 +0300

    Done

---
+++ Lesson_8/cart.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const cart = document.querySelector('.cart');
+const cartIcon = document.querySelector('.cartIconWrap');
+cartIcon.addEventListener('click', () => cart.classList.toggle('hidden'));
+
+const cartRows = {};
+const itemsEl = document.querySelector('.featuredItems');
+const cartHeader = document.querySelector('.cartHeader');
+const cartCounter = document.querySelector('.cartCounter');
+const cartTotalValue = document.querySelector('.cartTotalValue');
+
+itemsEl.addEventListener('click', clickHandler);
+
+function clickHandler(event) {
+    if (!event.target.classList.contains('toCart')) return;
+    document.querySelectorAll('.feturedRow').forEach(row => row.remove());
+    let product = {
+            id: event.target.parentNode.parentNode.parentNode.dataset.id,
+            name: event.target.parentNode.parentNode.parentNode.dataset.name,
+            price: event.target.parentNode.parentNode.parentNode.dataset.price,
+    }
+    addToCart(product);
+}
+
+
+function addToCart(product) {
+    getCartRows(product);
+    cartHeader.insertAdjacentHTML('afterend', buildCartRow(cartRows));
+}
+
+function getCartRows(product) {
+    if (cartRows.hasOwnProperty(product.id)) {
+        cartRows[product.id].counter++;
+        cartRows[product.id].total = cartRows[product.id].price * cartRows[product.id].counter;
+    } else {
+        cartRows[product.id] = {
+            id: product.id,
+            name: product.name,
+            price: +product.price,
+            counter: 1,
+            total: +product.price,
+        }
+    }
+}
+
+function buildCartRow(cartRows) {
+    let result = '';
+    let totalCounter = 0;
+    let totalValue = 0;
+    for (let key in cartRows) {
+        result += `<div class="cartRow feturedRow" data-id="${cartRows[key].id}">
+                    <div>${cartRows[key].name}</div>
+                    <div>${cartRows[key].counter} шт.</div>
+                    <div>$${(cartRows[key].price).toFixed(2)}</div>
+                    <div>$${(cartRows[key].total).toFixed(2)}</div>
+                </div>`
+        totalCounter += cartRows[key].counter;
+        totalValue += cartRows[key].total;
+    }
+    cartCounter.innerHTML = totalCounter;
+    cartTotalValue.innerHTML = totalValue.toFixed(2);
+    return result;
+}
